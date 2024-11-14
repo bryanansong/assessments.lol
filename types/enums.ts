20328097e91a950bbef91bae30796b52d3e5dfee
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-11-14 04:08:37 -0500

    exported enums

--- types/enums.ts
+++ types/enums.ts
@@ -1,12 +1,12 @@
-const ROLE_TYPES = ['INTERNSHIP', 'NEW_GRAD', 'FULL_TIME'] as const;
-const PLATFORMS = ['CODESIGNAL', 'HACKERRANK'] as const;
-const SUBMISSION_STATUSES = [
+export const ROLE_TYPES = ['INTERNSHIP', 'NEW_GRAD', 'FULL_TIME'] as const;
+export const PLATFORMS = ['CODESIGNAL', 'HACKERRANK'] as const;
+export const SUBMISSION_STATUSES = [
     'PENDING',
     'REJECTED',
     'MOVED_FORWARD',
     'AWAITING_RESPONSE'
   ] as const;
 
-type RoleType = typeof ROLE_TYPES[number];
-type Platform = typeof PLATFORMS[number];
-type SubmissionStatus = typeof SUBMISSION_STATUSES[number];
+export type RoleType = typeof ROLE_TYPES[number];
+export type Platform = typeof PLATFORMS[number];
+export type SubmissionStatus = typeof SUBMISSION_STATUSES[number];
