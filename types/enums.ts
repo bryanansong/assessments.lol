59ae5fbb47f510a28d81381c0a0fef3582bb8379
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-11-14 03:24:26 -0500

    updated enums

--- types/enums.ts
+++ types/enums.ts
@@ -1,5 +1,12 @@
 const ROLE_TYPES = ['INTERNSHIP', 'NEW_GRAD', 'FULL_TIME'] as const;
 const PLATFORMS = ['CODESIGNAL', 'HACKERRANK'] as const;
+const SUBMISSION_STATUSES = [
+    'PENDING',
+    'REJECTED',
+    'MOVED_FORWARD',
+    'AWAITING_RESPONSE'
+  ] as const;
 
 type RoleType = typeof ROLE_TYPES[number];
 type Platform = typeof PLATFORMS[number];
+type SubmissionStatus = typeof SUBMISSION_STATUSES[number];
